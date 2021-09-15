73e3263488a8b0dc85f738a9a13a6720f7863693
Michal Cwiekala <80964094+mcwiekala@users.noreply.github.com>
2021-09-15 10:41:48 +0200

    test for option

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ for (String value : sourceList) {
     }
 }
 ```
+## Java 8
 Since 8th version, Java introduced more declarative features which allows Functional Programming. So instead of describing each step with state, you are able to pass only behaviour. The similar code you can write with Java 8 in this way:
 ```java
 List<String> destinationList = sourceList.stream()
@@ -22,7 +23,7 @@ List<String> destinationList = sourceList.stream()
 * Lambda
 * Stream/primitive streams
 * Optional
-* Functional Interfaces (Supplier, Consumer, Predicate...  BiFunction etc.)
+* Functional Interfaces (Supplier, Consumer, Predicate, UnaryOperator...  BiFunction etc.)
 * Method references
 
 However these features are not ideal and it could be delivered in better way. That's the reason why Vavr was invented (formerly called Javaslang). 
@@ -31,7 +32,9 @@ However these features are not ideal and it could be delivered in better way. Th
 
 >Vavr is an object-functional language extension to Java 8, which aims to reduce the lines of code and increase code quality. It provides persistent collections, functional abstractions for error handling, concurrent programming, pattern matching and much more.
 
-https://github.com/vavr-io/vavr
+>Vavr is a new Guava
+
+[Check Git Repository](https://github.com/vavr-io/vavr)
 
 ## Project goals
 This is a simple project to check Vavr features. 
---
+++ src/test/java/io/micw/OptionTest.java
@@ -0,0 +1,76 @@
+package io.micw;
+
+import io.vavr.control.Option;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.Match;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OptionTest {
+
+    @Test
+    public void check_1() {
+        // given
+        String text = "text";
+        String defaultText = "default";
+        Option<String> name = Option.of(text);
+        Option<String> nullValue = Option.of(null);
+        // when ,then
+        assertThat(name.getOrElse(defaultText), equalTo(text));
+        assertThat(nullValue.getOrElse(defaultText), equalTo(defaultText));
+    }
+
+    @Test
+    public void check_2() {
+        // given
+        Integer result1 = getProcessText("17");
+        Integer result2 = getProcessText("3");
+
+        // when ,then
+        assertThat(result1, equalTo(17));
+        assertThat(result2, equalTo(-1));
+    }
+
+    @Test
+    public void check_3() {
+        // given
+        Class<UnsupportedOperationException> exceptionClass = UnsupportedOperationException.class;
+        String text = "text";
+        Optional<String> stringOptional = Optional.ofNullable(text);
+        Optional<String> stringOptional2 = Optional.empty();
+
+        Option<String> result1 = Option.ofOptional(stringOptional);
+        Exception exception = assertThrows(exceptionClass, () -> Option.ofOptional(stringOptional2)
+                .getOrElseThrow(UnsupportedOperationException::new));
+
+        // when ,then
+        assertThat(result1.getOrElse("default"), equalTo(text));
+        assertThat(exception.getClass(), equalTo(exceptionClass));
+    }
+
+    @Test
+    public void check_4() {
+        // given
+        AtomicReference<Boolean> bool = new AtomicReference<>(false);
+        Option<String> option = Option.none();
+        option.onEmpty(() -> bool.set(true));
+
+        // when ,then
+        assertThat(bool.get(), equalTo(true));
+    }
+
+    private Integer getProcessText(String numberAsText) {
+        return Option.of(numberAsText)
+                .map(Integer::parseInt)
+                .filter(integer -> integer > 5)
+                .getOrElse(-1);
+    }
+
+}
