0840783ccce8b917cb961b7ffde6c77c8871694e
Michal Cwiekala <80964094+mcwiekala@users.noreply.github.com>
2021-09-15 13:18:09 +0200

    test for functions

--- src/test/java/io/micw/FunctionalTest.java
+++ src/test/java/io/micw/FunctionalTest.java
@@ -1,10 +1,8 @@
 package io.micw;
 
+import io.vavr.Function0;
+import io.vavr.Function2;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -13,34 +11,64 @@ import static org.hamcrest.Matchers.is;
 class FunctionalTest {
 
     @Test
-    public void example_11() {
-        List<String> sourceList = new ArrayList<>();
-        List<String> destinationList = sourceList.stream()
-                .filter(word -> word.contains("Hello"))
-                .collect(Collectors.toList());
-        System.out.println(destinationList);
-
+    public void check_1() {
+        // given
+        String textNumber = "123";
+        Integer number1 = 123;
+        Integer number2 = 453;
+        Function2<String, Integer, Boolean> function = (text, integer) -> Integer.parseInt(text) == integer;
+        // when
+        Boolean result1 = function.apply(textNumber, number1);
+        Boolean result2 = function.apply(textNumber, number2);
+        // then
+        assertThat(result1, is(true));
+        assertThat(result2, is(false));
     }
 
     @Test
-    public void example_1() {
-        List<String> sourceList = List.of("Hello Java", "Hello Sir", "Kotlin", "Scala", "Hello Miss");
-        List<String> destinationList = new ArrayList<>();
-        for (String value : sourceList) {
-            if (value.contains("Hello")) {
-                destinationList.add(value);
-            }
-        }
-        System.out.println(destinationList);
-
-
-
-        boolean a = true;
-        boolean b = true;
-
-        assertThat(a, equalTo(b));
-        assertThat(a, is(equalTo(b)));
-        assertThat(a, is(b));
+    public void check_2() {
+        // given
+        String hello = "Hello";
+        String world = " world!";
+        Function0<String> function1 = () -> hello;
+        Function0<String> function2 = function1.andThen((text) -> text + world);
+        // when ,then
+        assertThat(function1.apply(), equalTo(hello));
+        assertThat(function2.apply(), equalTo(hello + world));
     }
 
+    @Test
+    public void check_3() {
+        // given check cache
+        Function0<Double> hashCache =
+                Function0.of(() -> {
+                            System.out.println("Processing");
+                            return Math.random();
+                        }
+                ).memoized();
+
+        // when
+        double randomValue1 = hashCache.apply();
+        double randomValue2 = hashCache.apply();
+
+        // then
+        assertThat(randomValue1, equalTo(randomValue2));
+    }
+
+    @Test
+    public void check_4() {
+        // given
+        Integer number1 = 2;
+        Integer number2 = 5;
+
+        Function2<Integer, Integer, Integer> subtraction = (a, b) -> a - b;
+
+        // when
+        Integer result1 = subtraction.apply(number1, number2);
+        Integer result2 = subtraction.reversed().apply(number1, number2);
+
+        // then
+        assertThat(result1, equalTo(-3));
+        assertThat(result2, equalTo(3));
+    }
 }
